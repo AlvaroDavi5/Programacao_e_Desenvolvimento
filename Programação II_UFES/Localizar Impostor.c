8c44a077ee9d7ca595df8b0053f7802eb6af5b56
Alvaro <alvaro-davi1@hotmail.com>
2020-10-12 15:47:47 -0300

    up

--- Programação II_UFES/Localizar Impostor.c
+++ Programação II_UFES/Localizar Impostor.c
@@ -1,45 +1,96 @@
+/*
+
+Verificar impostor
+
+Alvaro Davi, Eng Comp UFES, 2020
+
+*/
+
+//				~~~~~~ CREWMANTE ~~~~~~
+//			There is 1 impostor amon9 us 
+
 #include <stdio.h>
 #include <math.h>
 
 int main()
 {
-	int num, soma = 0;
+	int num;
+	int n1, n2, n3, n4;
 	int algU, algD, algC, algM;
 
 	scanf("%i", &num);
 
-	algU = (num % 10);
-	algD = (num % 100)/10;
-	algC = (num % 1000)/100;
-	algM = (num % 10000)/1000;
-
-	for (int i =0; i <= num; i++)
+	for (int i = 0; i <= 10000; i++)
 	{
+		algU = (num % 10);
+		algD = (num % 100)/10;
+		algC = (num % 1000)/100;
+		algM = (num % 10000)/1000;
 
 		if (num >= 0 && num < 10)
 		{
-			soma = (algU * algU);
+			n1 = algU;
+
+			n1 = n1 * n1;
+
+			num = n1;
 		}
 
 		else if (num >= 10 && num < 100)
 		{
-			soma = (algU*algU) + (algD*algD);
+			n1 = algU;
+			n2 = algD;
+
+			n1 = n1 * n1;
+			n2 = n2 * n2;
+
+			num = n1 + n2;
 		}
 
 		else if (num >= 100 && num < 1000)
 		{
-			soma = (algU*algU) + (algD*algD) + (algC*algC);
+			n1 = algU;
+			n2 = algD;
+			n3 = algC;
+
+			n1 = n1 * n1;
+			n2 = n2 * n2;
+			n3 = n3 * n3;
+
+			num = n1 + n2 + n3;
 		}
 
 		else if (num >= 1000 && num < 10000)
 		{
-			soma = (algU*algU) + (algD*algD) + (algC*algC) + (algM*algM);
+			n1 = algU;
+			n2 = algD;
+			n3 = algC;
+			n4 = algM;
+
+			n1 = n1 * n1;
+			n2 = n2 * n2;
+			n3 = n3 * n3;
+			n4 = n4 * n4;
+
+			num = n1 + n2 + n3 + n4;
 		}
 
 		else /* if (num < 0 || num >= 10000) */
 		{
 			printf("VALOR DEMASIADO PEQUENO/GRANDE");
 		}
+	}
+
+	if (num == 1)
+	{
+		// printf("%d \n", num);
+		printf("SIM");
+	}
+
+	else
+	{
+		// printf("%d \n", num);
+		printf("NAO");
 	}
 
 	return 0;
