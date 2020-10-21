f5218ac888125f1eb338c47e74906c6d286d3e06
Alvaro <alvaro-davi1@hotmail.com>
2020-10-21 10:25:00 -0300

    up

--- Programação II_UFES/Cálculo de Hipotenusa.c
+++ Programação II_UFES/Cálculo de Hipotenusa.c
@@ -10,13 +10,13 @@ mostrar Hip
 float main()
 {
     int CA, CO;
-    float Hip;
+    double Hip;
 
     printf("Digite o valor do cateto oposto:");
     scanf("%d", &CO);
     printf("Digite o valor do cateto adjacente:");
     scanf("%d", &CA);
     Hip = sqrt((CO*CO)+(CA*CA));
-    printf("O valor da hipotenusa é: %f \n",Hip);
+    printf("O valor da hipotenusa é: %lf \n",Hip);
     return 0;
 }
