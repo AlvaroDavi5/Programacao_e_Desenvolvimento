6ba558bd481339a363835e3cc258330242e19990
Alvaro <alvaro-davi1@hotmail.com>
2020-10-28 17:42:35 -0300

    Added example of structs and typedef

---
+++ Programação II_UFES/Teste de Struct com Função.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef struct /* tipo composto */
+{
+  int idade;
+  float nota; // tipos primitivos compondo o struct
+  int matricula;
+} tEstudante; // atribui o nome 'tEstudante' para o tipo composto struct (em escopo global) ao mesmo tempo em que serve de identificador ao struct referido
+
+void imprime(tEstudante aluno);
+
+int main()
+{
+  tEstudante aluno; // define o tipo da variável 'aluno' como o struct 'tEstudante'
+
+  aluno.idade = 18; // acessa a variável idade da variável aluno, definida no struct
+  aluno.nota = 9.5;
+  aluno.matricula = 20201010;
+
+  imprime(aluno);
+
+	return 0;
+}
+
+void imprime(tEstudante aluno) // variáável 'aluno' do tipo 'tEstudante' passada como parametro
+{
+  printf("%i %.2f %i", aluno.idade, aluno.nota, aluno.matricula);
+  return;
+}
