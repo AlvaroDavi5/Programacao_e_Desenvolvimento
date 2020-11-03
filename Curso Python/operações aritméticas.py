42b19875bbb3a3fa1df1d5010ea094cbf921997f
Alvaro <alvaro-davi1@hotmail.com>
2020-11-03 16:37:28 -0300

    up

--- Curso Python/operações aritméticas.py
+++ Curso Python/operações aritméticas.py
@@ -1,17 +1,18 @@
 num1 = input("digite um numero \n")
 num2 = input("digite outro numero \n")
-res
 
+
+print("\n")
 print("A concatenação entre os números é:", num1 + num2)
 
-res = num1 + num2
+res = float(num1) + float(num2)
 print("A soma entre os números é:", res)
 
-res = num1 - num2
+res = float(num1) - float(num2)
 print("A diferença entre os números é:", res)
 
-res = num1 * num2
+res = float(num1) * float(num2)
 print("O produto entre os números é:", res)
 
-res = num1 / num2
+res = float(num1) / float(num2)
 print("O quociente entre os números é:", res)
