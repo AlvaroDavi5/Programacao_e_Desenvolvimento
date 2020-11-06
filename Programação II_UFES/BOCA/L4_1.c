7c34b7230540dd93c959f3d19e2a6075808b8787
Alvaro <alvaro-davi1@hotmail.com>
2020-11-06 15:10:16 -0300

    up

--- Programação II_UFES/BOCA/L4_1.c
+++ Programação II_UFES/BOCA/L4_1.c
@@ -29,6 +29,7 @@ void ImprimeProduto(tProduto p);
 int main()
 {
 	int n;
+	tProduto pTemp, pMax, pMin;
 
 	scanf("%d", &n);
 
@@ -36,9 +37,119 @@ int main()
 	{
 		for (int i = 0; i < n; i++)
 		{
-			//
+			pTemp = LeProduto();
+
+			if (i == 0)
+			{
+				pMax = pTemp;
+				pMin = pTemp;
+			}
+
+			// FALTA
+			if (TemProdutoEmEstoque(pTemp))
+			{
+				// MAIOR
+				if (EhProduto1MaiorQ2(pMax, pTemp))
+				{
+					pMax = pTemp;
+				}
+
+				// MENOR
+				if (EhProduto1MenorQ2(pMin, pTemp))
+				{
+					pMin = pTemp;
+				}
+			}
+			else
+			{
+				printf("FALTA:");
+				ImprimeProduto(pTemp);
+
+				// MAIOR
+				if (EhProduto1MaiorQ2(pMax, pTemp))
+				{
+					pMax = pTemp;
+				}
+
+				// MENOR
+				if (EhProduto1MenorQ2(pMin, pTemp))
+				{
+					pMin = pTemp;
+				}
+			}
 		}
+
+		// MAIOR
+		printf("MAIOR:");
+		ImprimeProduto(pMax);	
+
+		// MENOR
+		printf("MENOR:");
+		ImprimeProduto(pMin);	
 	}
 
 	return 0;
+}
+
+tProduto LeProduto()
+{
+	int codigo, quantidade;
+	float preco;
+	tProduto prod;
+
+	scanf("%d;%f;%d", &codigo, &preco, &quantidade);
+
+	prod.COD = codigo;
+	prod.PRE = preco;
+	prod.QTD = quantidade;
+
+	return prod;
+}
+
+int EhProduto1MaiorQ2(tProduto p1, tProduto p2)
+{
+	if (p1.PRE >= p2.PRE)
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+int EhProduto1MenorQ2(tProduto p1, tProduto p2)
+{
+	if (p1.PRE <= p2.PRE)
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+int TemProdutoEmEstoque(tProduto p)
+{
+	if (p.QTD != 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+void ImprimeProduto(tProduto p)
+{
+	printf("COD ");
+	printf("%d, ", p.COD);
+	printf("PRE ");
+	printf("%.2f, ", p.PRE);
+	printf("QTD ");
+	printf("%d\n", p.QTD);
+
+	return;
 }
