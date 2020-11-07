da9401f70946ee23971ce55011d1c558a7d17421
Alvaro <alvaro-davi1@hotmail.com>
2020-11-07 15:36:40 -0300

    up

--- Programação II_UFES/BOCA/LDEP1_4.c
+++ Programação II_UFES/BOCA/LDEP1_4.c
@@ -9,64 +9,33 @@ Alvaro Davi, Eng Comp UFES, 2020
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-  int i = 0, den = 0, primo = 0, qtd = 0, cont = 0;
-
-  scanf("%d", &qtd);
-
-  for(i = 2; i <= qtd; i++)
-  {
-    while(den <= i)
-    {
-      if((cont == 2) || (cont == 3) || (cont == 5) || (cont == 7))
-      {
-        printf("%d ", cont);
-      }
-      cont++;
-
-      den++;
-    }
-
-    if((i % 2 != 0) && (i % 3 != 0) && (i % 5 != 0) && (i % 7 != 0))
-    {
-      printf("%d ", i);
-    }
-
-  }
-
-  return 0;
-}
-
-/*
-
-#include <stdio.h>
 
 int main()
 {
-  int i = 0, den = 0, primo = 0, qtd = 0, cont = 0;
+	int qtd, primo = 0, n = 2, i = 2;
 
-  scanf("%d", &qtd);
+	scanf("%d", &qtd);
 
-  for (i = 2; i < qtd; i++)
-  {
-    den += 2;
-    while(den < i)
-    {
-      if ((i % den) == 0)
-      {
-        cont++;
-        break;
-      }
-    }
+	for (n; n <= qtd; n++)
+	{
+		while (i < n)
+		{
+			if (n % i == 0)
+			{
+				primo++;
+			}
+			i++;
+		}
 
-    if ((cont % 2) == 0)
-    {
-      printf("%d", i);
-    }
-  }
+		i = 2;
 
-  return 0;
+		if (primo == 0)
+		{
+			printf("%d ", n);
+		}
+
+		primo = 0;
+	}
+
+	return 0;
 }
-
-*/
