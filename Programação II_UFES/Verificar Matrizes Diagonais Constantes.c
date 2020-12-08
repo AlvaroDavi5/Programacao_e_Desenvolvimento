b94f3fbdc8c10a10a5e5f957008761332dc327d9
Alvaro <alvaro-davi1@hotmail.com>
2020-12-07 21:25:27 -0300

    up

--- Programação II_UFES/Verificar Matrizes Diagonais Constantes.c
+++ Programação II_UFES/Verificar Matrizes Diagonais Constantes.c
@@ -4,21 +4,19 @@
 #define FALSE 0
 #define TRUE 1
 
-void verificarSeMatrizDiagonal(int matriz[100][100], int ordemMatriz);
-
 int main()
 {
-	int ordemMatriz;
+	int ordemMatriz, verif = FALSE, logic = TRUE, i, j;
 
 	scanf("%d%*c%*c", &ordemMatriz); // ler ordem (M linhas e N colunas) da matriz quadrada (M == N, NxN)
 
 	int matriz[ordemMatriz][ordemMatriz]; // definir dimensoes da matriz
 
 	scanf("%*c");
-	for (int i = 0; i < ordemMatriz; i++) // para acessar linhas
+	for (i = 0; i < ordemMatriz; i++) // para acessar linhas
 	{
 		scanf("%*c");
-		for (int j = 0; j < ordemMatriz; j++) // para preencher colunas
+		for (j = 0; j < ordemMatriz; j++) // para preencher colunas
 		{
 			scanf("%d%*c", &matriz[i][j]);
 		}
@@ -26,28 +24,12 @@ int main()
 	}
 	scanf("%*c");
 
-	verificarSeMatrizDiagonal(matriz, ordemMatriz);
+	// ALGORITMO PARA VERIFICAR (FEITO PARA A DIAGONAL PRINCIPAL, MAS FUNCIONANDO ATE O TESTE 5)
+	// -----------------------------------------------------------------------
 
-	for (int k = 0; k < ordemMatriz; k++)
+	for (i = 0; i < (ordemMatriz - 1) && logic; i++) // varrer linhas
 	{
-		for (int l = 0; l < ordemMatriz; l++)
-		{
-			printf("%d ", matriz[k][l]);
-		}
-		printf("\n");
-	}
-
-	return 0;
-}
-
-
-void verificarSeMatrizDiagonal(int matriz[100][100], int ordemMatriz)
-{
-	int verif = FALSE, logic = TRUE;
-
-	for (int i = 0; i < (ordemMatriz - 1) && logic; i++) // varrer linhas
-	{
-		for (int j = 0; j < (ordemMatriz - 1) && logic; j++) // varrer colunas
+		for (j = 0; j < (ordemMatriz - 1) && logic; j++) // varrer colunas
 		{
 			if (matriz[i][j] == matriz[i+1][j+1])
 			{
@@ -62,8 +44,7 @@ void verificarSeMatrizDiagonal(int matriz[100][100], int ordemMatriz)
 		}
 	}
 
-
-	if (verif != FALSE)
+	if (verif == TRUE)
 	{
 		printf("SIM");
 	}
@@ -71,4 +52,6 @@ void verificarSeMatrizDiagonal(int matriz[100][100], int ordemMatriz)
 	{
 		printf("NAO");
 	}
+
+	return 0;
 }
