c8131657aed778f32c139e35634ffc0090764acc
Alvaro Davi <alvaro-davi1@hotmail.com>
2020-12-20 22:16:30 -0300

    up

--- Curso Python/laços de repetição.py
+++ Curso Python/laços de repetição.py
@@ -2,18 +2,22 @@ index = 0
 
 while index <= 15:
 	print(index)
-	index+=1
-	pass
+	index += 1
 
 print ('\n')
 
-for x in range(12):
+
+
+for x in range(12): # gerar lista com valores de 0 a 12
+	if x == 0:
+		print("Zero ignorado")
+		continue # sai da iteração do loop temporarioamente
 	if x % 2 == 0:
 		print(x)
 
 print ('\n')
 
-for x in range(15, 50):
+for x in range(15, 50): # gerar lista com valores de 15 a 20
 	print(x)
 	if x == 25:
-		break
+		break # sai completamente do loop
