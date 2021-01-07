662399d651821b43f8f18f07ddcbed318b483eb8
Alvaro Davi <alvaro-davi1@hotmail.com>
2021-01-07 15:09:44 -0300

    up

--- Linguagem Python/objetos, classes, atributos, métodos, instâncias e propriedades da POO.py
+++ Linguagem Python/objetos, classes, atributos, métodos, instâncias e propriedades da POO.py
@@ -15,7 +15,7 @@ ENCAPSULAMENTO = encapsular os dados de uma aplicacao significa evitar que estes
 """
 
 
-# isto e uma classe
+''' isto e uma classe '''
 class Livro(): # para receber parametros usa-se parenteses
 
 	def __init__(self, isbn): # funcao (ou metodo, por estar dentro da classe) de inicaializacao - para inicializar cada objeto criado a partir desta classe
@@ -34,18 +34,18 @@ class Livro(): # para receber parametros usa-se parenteses
 
 
 
-# isto e um objeto (ou instancia da classe)
+''' isto e um objeto (ou instancia da classe) '''
 livro1 = Livro(9788594318725) # instanciando classe 'Livro()' ao criar objeto 'livro1' na classe e passando o valor do ISBN-13 para o parametro 'isbn'
 
 livro1.ano = 1912
 livro1.nome = "O Mundo Perdido" # definindo valores dos atributos do objeto
 livro1.genero = "ficção/fantasia"
 livro1.numpag = 240
-print(str(type(livro1)) + '\n')
+print(str(type(livro1)))
 
 
 
-# isto e um metodo
+''' isto e um metodo '''
 livro1.info() # chamada do metodo criado
 print("PAGINAS: " + str(len(livro1)) + '\n') # chamada do metodo especial 'len()'
 
@@ -54,7 +54,7 @@ if (hasattr(livro1, 'tipo_capa')): # verifica se tem atributo 'tipo_capa'
 
 
 
-# isto e heranca
+''' isto e heranca '''
 class Vertebrados(): # superclasse
 
 	def __init__(self):
@@ -78,7 +78,7 @@ class Cachorro(Vertebrados): # subclasse
 		self.familia = "canidae"
 
 	def latir(self):
-		print("Woolf!")
+		print("Woolf!\n")
 
 
 husky = Cachorro()
@@ -91,13 +91,56 @@ husky.latir() # isso e uma mensagem (ou chamada de metodo)
 
 
 
-# isto e polimorfismo
-#.
+''' isto e polimorfismo '''
+class FormaGeometrica(): # superclasse
+	def calc_area(self):
+		return "Erro no cálculo da área!"
+
+class Quadrado(FormaGeometrica): # subclasse
+
+	def __init__(self, base):
+		self.base = base
+
+	def calc_area(self):
+		return self.base * self.base
+
+
+class Circulo(FormaGeometrica): # subclasse
+
+	def __init__(self, raio):
+		self.raio = raio
+
+	def calc_area(self):
+		return self.raio * self.raio * 3.14
+
+forma1 = Quadrado(5)
+forma2 = Circulo(5)
+print("Forma 1 é um quadrado de base %s e área %s. \nForma 2 é um círculo de raio %s e área %s.\n" %(5, forma1.calc_area(), 5, forma2.calc_area()))
 
 
 
-# isto e encapsulamento
-#.
+''' isto e encapsulamento '''
+class InfoFracamentePrivada(): # exemplo de classe com itens fracamente privados
+	def __init__(self):
+		self._idade = 25
+ 
+	def _privmetod(self):
+		print("método fracamente privado")
+
+p1 = InfoFracamentePrivada()
+print(p1._idade)
+p1._privmetod()
 
 
+class InfoFortementePrivada(): # exemplo de classe com itens fortemente privados
+	def __init__(self):
+		self.__senha = 147256
+ 
+	def __privmetod(self):
+		print("método fortemente privado")
 
+p2 = InfoFortementePrivada()
+#print(p2.__senha) mensagem retornando erro, pois o atributo e fortemente privado
+print(p2._InfoFortementePrivada__senha) # agora deu certo
+#p2.__privmetod() mensagem retornando erro, pois o metodo e fortemente privado
+p2._InfoFortementePrivada__privmetod()
