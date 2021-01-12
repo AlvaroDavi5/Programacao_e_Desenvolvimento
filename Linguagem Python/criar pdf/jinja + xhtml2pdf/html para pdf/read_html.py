276ebf2404add3cf883a80dcfba06491b5102a9b
Alvaro Davi <alvaro-davi1@hotmail.com>
2021-01-12 15:20:55 -0300

    rebuild

---
+++ Linguagem Python/criar pdf/jinja + xhtml2pdf/html para pdf/read_html.py
@@ -0,0 +1,2 @@
+import xhtml2pdf
+
--- Linguagem Python/pacotes, módulos e bibliotecas.py
+++ Linguagem Python/pacotes, módulos e bibliotecas.py
@@ -12,10 +12,14 @@ import unicodedata # unicode convert data
 import wget # to dowload web files
 import requests # make requests of web pages
 import urllib # make requests of HTML pages
+import jinja2 # to make HTML template
+import sqlite3 # to use SQLite in Python
 import pandas as pd # to databases and data manipulation functions - 'pandas' library imported with alias 'pd'
 from functools import reduce # more function tools - from 'functools' library import 'reduce' function
 from fpdf import FPDF # to generate and manipulate PDF files = from 'fpdf' library import 'FPDF' class
 from reportlab.pdfgen import canvas # to generate and manipulate PDF files - from 'reportlab' package ('pdfgen' module) import 'canvas' library
+import pdfkit # to generate and manipulate PDF files
+import xhtml2pdf # to generate PDF based in a HTML file 
 from bs4 import Beautifulsoup # extract data from HTML file - from 'bs4' module import 'Beautifulsoup' library
 from selenium import webdriver # do autotests on browser - from 'selenium' package import 'webdriver' module
 from selenium.webdriver.firefox.options import Options # from 'selenium.webdriver.firefox.options' package path import 'Options' function
