829bfdc4bf4526e685402b93bada0e623173a4ca
Alvaro Davi <alvaro-davi1@hotmail.com>
2021-01-20 21:48:28 -0300

    up

--- Linguagem Python/manipulacao de dados/SQLite - database/main.py
+++ Linguagem Python/manipulacao de dados/SQLite - database/main.py
@@ -1,48 +1,19 @@
 import os
-import sqlite3 # import SQLite library
+import sqlite3
 
-# remove file if it exists
-if os.path.exists("./escola.db"):
-	os.remove("./escola.db")
-else:
-	None
+os.remove("dsa.db") if os.path.exists("dsa.db") else None
 
-# make connection with database, if database not exists, build database
-con = sqlite3.connect("escola.db")
+conn = sqlite3.connect("dsa.db")
+c = conn.cursor()
 
-# cursor to navigate to all registers on database
-cur = con.cursor()
+def createTable():
+	c.execute("CREATE TABLE IF NOT EXISTS produtos(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, date TEXT, prod_name TEXT, valor REAL)")
 
-# SQL instruction (DDL)
-sql_create_table = "create table cursos "\
-"(id integer primary key, "\
-"titulo varchar(100), "\
-"nota_mec integer, "\
-"centro_ensino varchar(140))" # unnecessary parse to uppercase
+def dataInsert():
+	c.execute("INSERT INTO produtos VALUES(10, 2018-05-02 14:32:12, 'Teclado', 90)")
+	conn.commit()
+	c.close()
+	conn.close()
 
-cur.execute(sql_create_table) # the cursos navigate in the SQL instruction and execute it
-
-# SQL instruction (DML)
-sql_insert_data = "insert into cursos values (?, ?, ?, ?)"
-
-recset = [
-	(1045, "Engenharia da Computacao", 5, "Centro Tecnologico"), 
-	(1018, "Direito", 5, "Centro de Ciencias Juridicas e Economicas"), 
-	(1024, "Matematica", 4, "Centro de Ciencias Exatas")
-] # to register an list of 3 tuples with 4 elements each
-
-for rec in recset:
-	cur.execute(sql_insert_data, rec) # registering
-
-# make commit on connection to record data transaction
-con.commit()
-
-# verify if data are recorded
-sql_select_all = "select * from cursos"
-cur.execute(sql_select_all)
-dataset = cur.fetchall()
-for data in dataset:
-	print("Id: %d \nCurso: %s \nNota do MEC: %d \nCentro de Ensino: %s" %(data))
-	print()
-
-con.close() # closing connection
+createTable()
+dataInsert()
