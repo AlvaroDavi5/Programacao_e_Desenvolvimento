c7035117a336b49c33a656583226846184a2b63d
Alvaro Davi <alvaro-davi1@hotmail.com>
2021-01-23 18:16:54 -0300

    up

--- Linguagem Python/pacotes, módulos e bibliotecas.py
+++ Linguagem Python/pacotes, módulos e bibliotecas.py
@@ -14,6 +14,8 @@ import wget # to dowload web files
 import requests # make requests of web pages
 import urllib # make requests of HTML pages
 import jinja2 # to make HTML template
+import PyV8 # bridge between the Python and JavaScript using Google V8 engine
+import js2py # translate JavaScript code to Python code
 import sqlite3 # to use SQLite in Python
 import pandas as pd # to databases and data manipulation functions - 'pandas' library imported with alias 'pd'
 from functools import reduce # more function tools - from 'functools' library import 'reduce' function
@@ -21,7 +23,7 @@ from fpdf import FPDF # to generate and manipulate PDF files = from 'fpdf' libra
 from reportlab.pdfgen import canvas # to generate and manipulate PDF files - from 'reportlab' package ('pdfgen' module) import 'canvas' library
 import pdfkit # to generate and manipulate PDF files
 import xhtml2pdf # to generate PDF based in a HTML file 
-from bs4 import Beautifulsoup # extract data from HTML file - from 'bs4' module import 'Beautifulsoup' library
+from bs4 import Beautifulsoup # extract data from HTML file, navigate to DOM tree - from 'bs4' module import 'Beautifulsoup' library
 from selenium import webdriver # do autotests on browser - from 'selenium' package import 'webdriver' module
 from selenium.webdriver.firefox.options import Options # from 'selenium.webdriver.firefox.options' package path import 'Options' function
 
