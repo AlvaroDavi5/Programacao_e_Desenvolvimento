d39c22019334222c7b6d8e013f7928b1910d38fd
Alvaro Davi <alvaro-davi1@hotmail.com>
2021-01-25 23:09:39 -0300

    up

--- Linguagem Python/pacotes, módulos e bibliotecas.py
+++ Linguagem Python/pacotes, módulos e bibliotecas.py
@@ -5,6 +5,7 @@ import json # to manipulate JSON files
 import pdfplumber # to manipulate PDF files
 import os # to use operating systems features
 import sys # to interage with the system
+import types # to dynamic creation of new types
 import random # make random selection or get random value
 import time # timeset and timedef library
 import datetime # timeset and timedef library
@@ -14,8 +15,10 @@ import wget # to dowload web files
 import requests # make requests of web pages
 import urllib # make requests of HTML pages
 import jinja2 # to make HTML template
+import execjs # use NodeJS to run JavaScript commands
 import PyV8 # bridge between the Python and JavaScript using Google V8 engine
 import js2py # translate JavaScript code to Python code
+import jiphy # JavaScript -> Python and Python -> JavaScript conversor
 import sqlite3 # to use SQLite in Python
 import pandas as pd # to databases and data manipulation functions - 'pandas' library imported with alias 'pd'
 from functools import reduce # more function tools - from 'functools' library import 'reduce' function
@@ -26,6 +29,7 @@ import xhtml2pdf # to generate PDF based in a HTML file
 from bs4 import Beautifulsoup # extract data from HTML file, navigate to DOM tree - from 'bs4' module import 'Beautifulsoup' library
 from selenium import webdriver # do autotests on browser - from 'selenium' package import 'webdriver' module
 from selenium.webdriver.firefox.options import Options # from 'selenium.webdriver.firefox.options' package path import 'Options' function
+
 
 
 print(dir(unicodedata))
