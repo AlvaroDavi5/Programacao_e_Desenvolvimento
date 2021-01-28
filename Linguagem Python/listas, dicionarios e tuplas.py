9aa7473cd7cbadcf97d7493147c9fdcbd06f8607
Alvaro Davi <alvaro-davi1@hotmail.com>
2021-01-27 22:35:48 -0300

    up

--- Linguagem Python/listas, dicionarios e tuplas.py
+++ Linguagem Python/listas, dicionarios e tuplas.py
@@ -1,5 +1,5 @@
 """ isso e uma lista """
-nomes_idades = ["Alvaro", 18, "Davi", 17, "Hanna", 15] # elementos e seus valores com indices e chaves automaticamente ordenados
+nomes_idades = ["Alvaro", 18, "Davi", 17, "Hanna", 15] # elementos e seus valores/conteudos com indices e chaves automaticamente ordenados
 
 """ isso e um dicionario """
 estudantesIdades = {"Matheus":12, "João":37} # elementos com chaves escolhidas (indices se mantem de forma implicita)
