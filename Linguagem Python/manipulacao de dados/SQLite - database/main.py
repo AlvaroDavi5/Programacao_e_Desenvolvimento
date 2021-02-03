4933c1a3e110bc0806bfcbf39101974302d0da3e
Alvaro Davi <alvaro-davi1@hotmail.com>
2021-02-03 01:38:23 -0300

    up

--- Linguagem Python/manipulacao de dados/SQLite - database/main.py
+++ Linguagem Python/manipulacao de dados/SQLite - database/main.py
@@ -1,4 +1,7 @@
 import os
+from datetime import datetime
+import time
+import random
 import sqlite3
 
 os.remove("dsa.db") if os.path.exists("dsa.db") else None
@@ -7,13 +10,26 @@ conn = sqlite3.connect("dsa.db")
 c = conn.cursor()
 
 def createTable():
-	c.execute("CREATE TABLE IF NOT EXISTS produtos(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, date TEXT, prod_name TEXT, valor REAL)")
+	c.execute(
+		"CREATE TABLE IF NOT EXISTS produtos" \
+		"(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, date TEXT, prod_name TEXT, valor REAL)"
+	)
 
 def dataInsert():
 	c.execute("INSERT INTO produtos VALUES(10, '2018-05-02 14:32:12', 'Teclado', 90)")
 	conn.commit()
-	c.close()
-	conn.close()
 
+def data_insert_var():
+	new_date = datetime.now()
+	new_prod_name = 'Placa de Vídeo'
+	new_valor = random.randrange(300, 900)
+	c.execute("INSERT INTO produtos (date, prod_name, valor) VALUES (?, ?, ?)", (new_date, new_prod_name, new_valor))
+	conn.commit()
+
+# finalizacao
 createTable()
 dataInsert()
+for i in range(5):
+	data_insert_var()
+c.close()
+conn.close()
